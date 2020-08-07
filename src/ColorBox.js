26ad0e71fdc0c4f831db53c8aebc490d01bbbef2
John Butler <johnmichaelbutler@yahoo.com>
2020-08-07 09:53:11 -0500

    Style ColorBox

---
+++ src/ColorBox.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import './ColorBox.css';
+
+function ColorBox(props) {
+  const { name, backgroundColor } = props;
+  return (
+    <div style={{background: backgroundColor}} className="ColorBox">
+      <div className="copy-container">
+        <div className="box-content">
+          <span>{name}</span>
+        </div>
+        <button className="copy-button">
+          Copy
+        </button>
+      </div>
+      <span className="see-more">More</span>
+    </div>
+  )
+};
+
+export default ColorBox
--- src/Palette.js
+++ src/Palette.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import ColorBox from './ColorBox';
+
+import './Palette.css';
 
 function Palette(props) {
+  const colorBoxes = props.colors.map(color => (
+    <ColorBox backgroundColor={color.color} name={color.name} />
+  ))
+
   return (
     <div className="Palette">
       <div className="Palette-colors">
-        <div>
-
-        </div>
+        {colorBoxes}
       </div>
     </div>
   )
