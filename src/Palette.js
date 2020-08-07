790615e72c894cf109d7c97c05e7fa042e62f977
John Butler <johnmichaelbutler@yahoo.com>
2020-08-07 08:58:56 -0500

    Created the Palette component

---
+++ src/Palette.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Palette(props) {
+  return (
+    <div className="Palette">
+      <div className="Palette-colors">
+        <div>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Palette;
